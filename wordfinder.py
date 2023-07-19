cb32ae5e23cde7d4d824e6d78b48729279ba6056
Nepalimolly <121688585+Nepalimolly@users.noreply.github.com>
2023-07-19 14:51:33 -0500

    commit

---
+++ wordfinder.py
@@ -0,0 +1,58 @@
+"""Word Finder: finds random words from a dictionary."""
+
+import random
+class WordFinder:
+    """Find random words from dictionary
+    >>> wf = WordFinder("simple.txt")
+    3 words read
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+
+    >>> wf.random() in ["cat", "dog", "porcupine"]
+    True
+    
+    """
+
+    def __init__(self,path):
+        """Read and repreats function on dictionary"""
+
+        dict_file = open(path)
+
+        self.words = self.parse(dict_file)
+
+        print(f"{len(self.words)} words read")
+
+    def parse(self, dict_file):
+        """Parse the words"""
+        return [w.strip() for w in dict_file]
+    
+    def random(self):
+        """Return random Word"""
+        return random.choice(self.words)
+    
+
+class SpecialWordFinder(WordFinder):
+    """Specialized WordFinder that excludes blank lines/comments.
+    
+    >>> swf = SpecialWordFinder("complex.txt")
+    3 words read
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+
+    >>> swf.random() in ["pear", "carrot", "kale"]
+    True
+    """
+
+    def parse(self, dict_file):
+        """Parse dict_file -> list of words, skipping blanks/comments."""
+
+        return [w.strip() for w in dict_file
+                if w.strip() and not w.startswith("#")]
